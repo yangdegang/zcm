29f7e393d977114d07133ff6456aa25fdc992833
Jonathan Bendes <jonathan@skyspecs.com>
2015-10-24 14:50:48 -0400

    Starting to work on webspy

---
+++ tools/webspy/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+app.use(express.static("public"));
+
+// var zcm = require('zcm');
+// var zcmtypes = require('./zcmtypes');
+// var z = zcm.create(http, zcmtypes, 'ipc');
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
