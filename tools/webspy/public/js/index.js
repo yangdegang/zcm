dc0ee5d3a1c62ce9cc670a45525b87eea3d98cb7
Jonathan Bendes <jonathan@skyspecs.com>
2015-10-29 18:58:41 -0400

    Fixed hertz calculation

--- tools/webspy/public/js/index.js
+++ tools/webspy/public/js/index.js
@@ -8,20 +8,25 @@ var lastIdx = null;
 function handle(channel, msg)
 {
     var utime = Date.now() * 1000;
-    var freq = 0;
 
     if (!(channel in channelIdx)) {
         channelIdx[channel] = messages.length;
         messages.push({});
-    } else {
-        var freq_now = 1000000 / (utime - messages[channelIdx[channel]].utime);
-        freq = messages[channelIdx[channel]].frequency * 0.1 + freq_now * 0.9;
     }
+
+    var freq = 0;
+    if ("frequency" in messages[channelIdx[channel]])
+        freq = Number(messages[channelIdx[channel]].frequency);
+
+    var lastUtime = utime;
+    if ("utime" in messages[channelIdx[channel]])
+        lastUtime = messages[channelIdx[channel]].utime;
 
     messages[channelIdx[channel]] = {channel: channel,
                                      type: msg.__type,
                                      frequency: freq.toFixed(2),
                                      utime: utime,
+                                     lastUtime: lastUtime,
                                      msg: msg};
 
     var source   = $("#messages-template").html();
@@ -52,6 +57,39 @@ function showChannel(channel)
     }
 }
 
+function calcHertzLoop()
+{
+    setTimeout(function() {
+        for (var m in messages) {
+            if (messages[m]["utime"] != messages[m]["lastUtime"]) {
+
+                var dt = messages[m]["utime"] - messages[m]["lastUtime"];
+                var dtPredict = Date.now() * 1000 - messages[m]["utime"];
+
+                var freq;
+                if (dtPredict > dt * 3) {
+                    freq = 0;
+                } else if (dtPredict > dt) {
+                    freq = 1000000 / dtPredict;
+                } else {
+                    freq = 1000000 / dt;
+                }
+
+                messages[m]["frequency"] = freq.toFixed(2);
+
+                var source   = $("#messages-template").html();
+                var template = Handlebars.compile(source);
+                var data = { messages: messages }
+                var res = template(data);
+                $(res).css("background-color", "red");
+                $("#message-table").html(res);
+
+            }
+        }
+        calcHertzLoop();
+    }, 500);
+}
+
 var subscriptions = [];
 
 onload = function()
@@ -60,5 +98,5 @@ onload = function()
     subscriptions.push({channel: ".*",
                         subscription: z.subscribe_all(handle)});
 
-    setTimeout(recalculateHertz, 1000);
+    calcHertzLoop();
 }
